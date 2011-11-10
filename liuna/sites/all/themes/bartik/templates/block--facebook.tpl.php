651001dcd428af871c60194e28047c6ff25a168c
TheMagical C0de UnicOrn <james@fifthroomcreative.com>
2011-11-10 11:06:37 -0500

    initial

---
+++ liuna/sites/all/themes/bartik/templates/block--facebook.tpl.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Facebook Like Box Block
+ *
+ * This is straight XFBML, with no editable bits except for the tag attributes. It requires
+ * the Facebook JS SDK to be included directly below the opening <body> tag in html.tpl.php.
+ *
+ * See http://developers.facebook.com/docs/reference/plugins/like-box/
+ */
+
+?>
+<fb:like-box href="http://www.facebook.com/pages/LiUNA-Eastern-Region/224388557595313" width="292" show_faces="true" stream="true" header="true"></fb:like-box>
