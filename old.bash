fdec766c6d57f6481f219bdf1b92fa623fd93fc7
TheMagical C0de UnicOrn <james@fifthroomcreative.com>
2011-11-11 21:40:04 -0500

    changed name

---
+++ old.bash
@@ -0,0 +1,6 @@
+scp jamesandino@fifthroom.dnsdojo.com:Volumes/FRC500/Vault/_Current/Verge180/086-11-NJLWebsite/archive_old.tar.gz ./
+tar -zxvf ./archive_old.tar.gz
+rm  ./archive_old.tar.gz
+
+
+#LQ6mp(3z)n
