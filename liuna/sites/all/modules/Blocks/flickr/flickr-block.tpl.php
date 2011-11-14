5d663e204b41a9c4f2f65c3ae0ed6a83a824c651
Mike Green <mike.is.green@gmail.com>
2011-11-14 13:59:24 -0500

    Moved flickr block template into its block folder, wrote a hook_theme function to define default vars

--- liuna/sites/all/modules/Blocks/flickr/flickr-block.tpl.php
+++ liuna/sites/all/modules/Blocks/flickr/flickr-block.tpl.php
@@ -4,5 +4,9 @@
  *
  * Renders a Flickr feed (as serialized PHP) into a grid of thumbnails with links to the original image.
  */
-pretty_r($variables);
+if (isset($items)) {
+	pretty_r($items);
+} else {
+	pretty_r("No items var");
+}
 ?>
