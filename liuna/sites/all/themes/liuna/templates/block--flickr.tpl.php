4297180c237f7389f543768d7ddcf152f948dae3
TheMagical C0de UnicOrn <james@fifthroomcreative.com>
2011-11-14 20:11:58 -0500

    moved @mikegdamages work to proper theme directory

---
+++ liuna/sites/all/themes/liuna/templates/block--flickr.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Flickr Photo Grid Block
+ *
+ * Renders a Flickr feed (as serialized PHP) into a grid of thumbnails with links to the original image.
+ */
+?>
+<h1>I'm a Flickr</h1>
+<?php echo $variables['elements']['#markup']; ?>
