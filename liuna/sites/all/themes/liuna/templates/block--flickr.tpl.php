99982a1c728f884331277462f9b0d6785c5d77d3
Mike Green <mike.is.green@gmail.com>
2011-11-21 13:31:59 -0500

    Added front-end template for the slider block to the liuna theme

--- liuna/sites/all/themes/liuna/templates/block--flickr.tpl.php
+++ liuna/sites/all/themes/liuna/templates/block--flickr.tpl.php
@@ -5,5 +5,5 @@
  * Renders a Flickr feed (as serialized PHP) into a grid of thumbnails with links to the original image.
  */
 ?>
-<h1>I'm a Flickr</h1>
+<h1>Flickr Feed</h1>
 <?php echo $variables['elements']['#markup']; ?>
---
+++ liuna/sites/all/themes/liuna/templates/block--slider.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Slider Block Template
+ *
+ * Renders a JavaScript slider, powered by AnythingSlider library
+ */
+?>
+<?php echo $variables['elements']['#markup']; ?>
