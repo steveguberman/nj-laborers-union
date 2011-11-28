a862985b2043abaf1c882e7e134120012f4a7b4c
Mike Green <mike.is.green@gmail.com>
2011-11-28 16:07:45 -0500

    Removed individual block template from flickr block, going to build a preprocess hook instead

--- liuna/sites/all/modules/Blocks/flickr/flickr_thumbnail_grid.tpl.php
+++
@@ -1,9 +0,0 @@
-<ul id="flickr-thumbnail-grid">
-	<?php foreach ($items['items'] as $photo): ?>
-		<li class="flickr-thumb">
-			<a href="<?php echo $photo['url']; ?>" class="photo-wrap <?php echo $photo['class']; ?>">
-				<img src="<?php echo $photo['source']; ?>" width="<?php echo $photo['width'] / 2; ?>" height="<?php echo $photo['height'] / 2; ?>" alt="<?php echo $photo['label']; ?>" />
-			</a>
-		</li>
-	<?php endforeach; ?>
-</ul>
