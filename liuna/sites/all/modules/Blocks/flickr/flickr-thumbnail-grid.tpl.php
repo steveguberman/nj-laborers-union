225e5272f633176607d423248174f9b68529400b
Mike Green <mike.is.green@gmail.com>
2011-11-29 11:37:03 -0500

    Got preprocessing hooks and file-based templates working on the Flickr block

--- liuna/sites/all/modules/Blocks/flickr/flickr-thumbnail-grid.tpl.php
+++ liuna/sites/all/modules/Blocks/flickr/flickr-thumbnail-grid.tpl.php
@@ -1,10 +1,10 @@
 <ul id="flickr-thumbnail-grid">
-	<?php foreach ($items as $thumb): ?>
+	<?php pretty_r($photos); ?>
+	<?php foreach ($photos as $thumb): ?>
 		<?php
 		$width = $thumb['width'] / 2;
 		$height = $thumb['height'] / 2;
 		?>
-		<?php echo $foobar; ?>
 		<li class="flickr-thumb">
 			<a href="<?php echo $thumb['url']; ?>" class="photo-wrap <?php echo $thumb['class']; ?>">
 				<img src="<?php echo $thumb['source']; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" alt="<?php echo $thumb['label']; ?>"/>
