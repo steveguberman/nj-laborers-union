b8561547d3c8a9a002f3a57223c365f0cc6d33ce
Mike Green <mike.is.green@gmail.com>
2011-12-01 11:33:29 -0500

    Wrote preprocess function for individual slider block items

---
+++ liuna/sites/all/modules/Blocks/slider/slider-block-item.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Slider Block Item Template
+ *
+ * Available variables:
+ * - $title: Title of the slide.
+ * - $caption: A longer description of the slide.
+ * - $src: URL of the slide's full-size image.
+ * - $thumb: URL of the full-size image's thumbnail version.
+ * - $href: URL that the slide should link to.
+ */
+?>
