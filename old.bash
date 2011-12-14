385404276765b3169ad34aa1428c3fcb2e0a1f5f
TheMagical C0de UnicOrn <james@fifthroomcreative.com>
2011-12-13 19:42:17 -0500

    useless

--- old.bash
+++
@@ -1,6 +0,0 @@
-scp jamesandino@fifthroom.dnsdojo.com:Volumes/FRC500/Vault/_Current/Verge180/086-11-NJLWebsite/archive_old.tar.gz ./
-tar -zxvf ./archive_old.tar.gz
-rm  ./archive_old.tar.gz
-
-
-#LQ6mp(3z)n
